6da22f4383657aa142d0791f1dfcd6bebc69a97f
Ahsanmedia <168821488+Ahsanmedia@users.noreply.github.com>
2024-05-03 11:01:36 -0700

    first commit

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"=:g CO2(Pf v`rr;DI$j[.8(:yYqTz{)1Pp_Yve|$T1Kk>W{q~?%p~b)(evs.#&Y";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3241;}
